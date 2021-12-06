17278a2be4bf2ac87fc69c344f4e201512051bde
Sai Nallani <74580408+sai-nallani@users.noreply.github.com>
2021-12-06 17:37:59 +0000

    made food items shit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
 import pprint
-
+import smtplib
 from scraper import WebScraper
 import json
 
@@ -19,19 +19,26 @@ months = ["January",
           "November",
           "December"]
 month = months[dt.month - 1]
+day = dt.strftime("%A %-d")
 school_value = "7001"
 # endregion
 
-scraper = WebScraper()
-scraper.set_parameters(school_value, month, menu="Lunch")
+def dump_in_json():
+  scraper = WebScraper()
+  scraper.set_parameters(school_value, month, menu="Lunch")
 
-dates, food_items = scraper.scrape_all()
-menu = {}
-for i in range(len(food_items) - 1):
-    food_list = food_items[i].text
-    new_food_str = food_list.replace("w/", 'with')
-    menu[dates[i]] = new_food_str
-scraper.quit()
-with open("menu.json", 'w') as file:
-    json.dump({month: menu}, file)
-pprint.pprint({month: menu})
+  dates, food_items = scraper.scrape_all()
+  menu = {}
+  for i in range(len(food_items) - 1):
+      food_list = food_items[i].text
+      new_food_str = food_list.replace("w/", 'with')
+      menu[dates[i]] = new_food_str
+  scraper.quit()
+  with open("menu.json", 'w') as file:
+      json.dump({month: menu}, file)
+
+with open('menu.json', 'r') as file:
+  menu_json = json.load(file)
+  food_items = menu_json[month][day]
+
+#configure smtp settings
